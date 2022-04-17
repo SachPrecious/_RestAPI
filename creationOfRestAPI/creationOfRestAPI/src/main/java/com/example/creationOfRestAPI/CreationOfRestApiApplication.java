27422170645441fee2f56be7ab4692dcbe987575
SachPrecious <sachithrabandara49@gmail.com>
2022-04-17 22:38:07 +0530

    Init commit

---
+++ creationOfRestAPI/creationOfRestAPI/src/main/java/com/example/creationOfRestAPI/CreationOfRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.creationOfRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationOfRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationOfRestApiApplication.class, args);
+	}
+
+}
---
+++ creationOfRestAPI/creationOfRestAPI/src/test/java/com/example/creationOfRestAPI/CreationOfRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creationOfRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationOfRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
